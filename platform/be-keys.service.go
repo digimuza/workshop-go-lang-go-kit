fa9adad45e00496bea9bc177810c1557fc7a90d2
andrius <andrius.mozuraitis@adeoweb.lt>
2017-12-29 14:01:55 +0200

    Add assigments

--- platform/be-keys.service.go
+++ platform/be-keys.service.go
@@ -1,3 +1,15 @@
 package main
 
 // TODO: Create BeKeysService interface and implemantation
+
+// TODO: BeKeyPair Definition.
+
+// TODO: BeKeyService interface.
+//	GetKeyPair method
+// 	AddKeyPair method -- skip
+//	DeleteKeyPair method -- skip
+//	UpdateKeyPair method -- skip
+
+// TODO: beKeyService struct implementation - actual business logic
+
+//Implement BeKeysService interface
